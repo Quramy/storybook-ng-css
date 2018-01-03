8778670b55f1d26ec2aa7b90fdf84e1e4709aedb
y-kurami <yosuke.kurami@gmail.com>
2018-01-03 20:40:07 +0900

    add comment

--- src/app/my-button/my-button.component.ts
+++ src/app/my-button/my-button.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-my-button',
   templateUrl: './my-button.component.html',
+  // use `styleUrls`: styles are not applied...
   // styleUrls: ['./my-button.component.css'],
+  // use `styles`: red color applied
   styles: [`
     p {
       color: red;
